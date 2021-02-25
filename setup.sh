df1a0c0100f3b0c8595a5a3df646e1fcd85a1f04
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2021-02-25 16:19:59 +0100

    feat(setup): explicit folder manipulations

--- setup.sh
+++ setup.sh
@@ -27,12 +27,15 @@ chmod +x $dir/rollout.sh
 chmod +x $dir/start.sh
 chmod +x $dir/rollback.sh
 
-rm -rf $DEPLOY_DIR
-mkdir $DEPLOY_DIR
+rm -rf $DEPLOY_DIR/next
+rm -rf $DEPLOY_DIR/app
+rm -rf $DEPLOY_DIR/prev 
+rm -rf $DEPLOY_DIR/logs
 
 mkdir $DEPLOY_DIR/next
 mkdir $DEPLOY_DIR/app
 mkdir $DEPLOY_DIR/prev
+mkdir $DEPLOY_DIR/logs
 
 echo "
 
